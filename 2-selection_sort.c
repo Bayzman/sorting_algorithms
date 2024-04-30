0a17321741f91861e729ff15f5013087ba6a49a6
goody-1 <atundeg@gmail.com>
2024-04-30 12:33:11 +0100

    convert indentation to tabs

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,27 +10,27 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int min, temp, temp2;
+	size_t i, j;
+	int min, temp, temp2;
 
-    for (i = 0; i < size; i++)
-    {
-        min  = array[i];
+	for (i = 0; i < size; i++)
+	{
+		min  = array[i];
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < min)
-            {
-                min = array[j];
-                temp = j;
-            }
-        }
-        if (min != array[i])
-        {
-            temp2 = array[i];
-            array[i] = min;
-            array[temp] = temp2;
-            print_array(array, size);
-        }
-    }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				temp = j;
+			}
+		}
+		if (min != array[i])
+		{
+			temp2 = array[i];
+			array[i] = min;
+			array[temp] = temp2;
+			print_array(array, size);
+		}
+	}
 }
